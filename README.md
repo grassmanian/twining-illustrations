60bac7b220785819528f442f73ef7998e27f3d9e
N V <52893991+grassmanian@users.noreply.github.com>
2023-05-09 14:52:22 -0400

    Working implementation of scraper

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Introduction
 
 Quick script to download all 160 of the botanical illustrations from https://www.c82.net/twining
+
+Use by installing dependencies, and running `python scraper.py`
+
+# Dependencies
+
+Requests, BeautifulSoup4
+
+# Known issues
+
+Does not correctly handle Pittosporaceæ and Jasmineaceæ, recommending manual intervention for these cases. (It seems the files are misnamed on the server...)
+
--- scraper.py
+++ scraper.py
@@ -1,8 +1,25 @@
 import requests
+from bs4 import BeautifulSoup
+import unicodedata
 
-image_path = "https://www.c82.net/images/twining/large/ranunculaceae.jpg"
-  
-image = requests.get(image_url) 
-  
-with open("ranunculaceae.jpg",'wb') as fout:
-    fout.write(r.content)
+# Parse out list of plant names
+table_page = requests.get('https://www.c82.net/twining/plants/?view=illustrated')
+soup = BeautifulSoup(table_page.content, 'html.parser')
+
+indices = [x for x in range(321) if x % 2 == 1]
+pre_formatted_names = [soup.ol.contents[x].span.contents[0] for x in indices]
+names = [''.join(filter(str.isalpha, x)) for x in pre_formatted_names]
+ascii_names = [x.replace('æ', 'ae').lower() for x in names]
+
+# Write download and write image
+for item in ascii_names:
+    image_url = 'https://www.c82.net/images/twining/large/{}.jpg'.format(item)
+    image = requests.get(image_url) 
+    try: 
+        image.raise_for_status()
+    except:
+        print('Error downloading {}, status {}'.format(item, image))
+        continue
+    else:
+        with open("{}.jpg".format(item),'wb') as fout:
+            fout.write(image.content)
